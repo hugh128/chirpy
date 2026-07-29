66f876164b87f4a39ca2e512b7d79ea8e6e24ae9
Hugh <hugh128ord@gmail.com>
2026-07-29 16:52:56 -0600

    Add fileserver and gitignore

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	const port = "8080"
 
+	fileServerHandler := http.FileServer(http.Dir("."))
+
 	mux := http.NewServeMux()
+	mux.Handle("/", fileServerHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
