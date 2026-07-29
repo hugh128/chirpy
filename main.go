5543a920bf6d8860ae2260368b4b8ac3d694cdc4
Hugh <hugh128ord@gmail.com>
2026-07-29 17:55:55 -0600

    Added handler

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
-	fileServerHandler := http.FileServer(http.Dir("."))
+	fileServerHandler := http.FileServer(http.Dir(filepathRoot))
 
 	mux := http.NewServeMux()
-	mux.Handle("/", fileServerHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", fileServerHandler))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +22,10 @@ func main() {
 
 	log.Printf("Server running on port: %v\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
